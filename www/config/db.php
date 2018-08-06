8451139583aec85161f5a92a06457665c6e03e33
se1209 <sergeev.evgenij@gmail.com>
2018-08-06 18:26:35 +0300

    Подключение к БД

--- www/config/db.php
+++ www/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=yii2_shop',
+    'username' => 'yii2_shop_user',
+    'password' => '1w6APxvAr3IiOHa5',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
