a6dcd7b96bce3a050178cb5595bb36319274b4d8
se1209 <sergeev.evgenij@gmail.com>
2018-08-19 17:28:51 +0300

    RBAC начало. Пока комичу с ошибкой.

--- www/commands/RbacController.php
+++ www/commands/RbacController.php
@@ -28,9 +28,42 @@ class RbacController extends Controller
         $admin = $auth->createRole('admin');
 
         // Записываем роли в БД;
-        $auth->add($user);
+        //$auth->add($user);
         $auth->add($courier);
         $auth->add($manager);
         $auth->add($admin);
+
+        // Добавляем и описываем роли;
+        /*$registration = $auth->createPermission('registration');
+        $registration->description = 'Регистрация';*/
+
+        $adminPage = $auth->createPermission('adminPage');
+        $adminPage->description = 'Просмотр админки';
+
+        $viewPanelCourier = $auth->createPermission('viewPanelCourier');
+        $viewPanelCourier->description = 'Панель курьера';
+
+        $viewPanelManager = $auth->createPermission('viewPanelManager');
+        $viewPanelManager->description = 'Панель менеджера';
+
+        // Записываем роли в БД;
+        $auth->add($adminPage);
+        $auth->add($viewPanelCourier);
+        $auth->add($viewPanelManager);
+
+        // Присваеваем разрешения ролям;
+        $auth->addChild($courier, $viewPanelCourier);
+        $auth->addChild($manager, $viewPanelManager);
+
+        // Наследуем роли;
+        $auth->addChild($manager, $courier);
+        $auth->addChild($admin, $manager);
+
+        // Разрешения админа;
+        $auth->addChild($admin, $adminPage);
+
+        // Прописываем админа и менеджера;
+        $auth->assign($admin, 1);
+        $auth->assign($manager, 6);
     }
 }
